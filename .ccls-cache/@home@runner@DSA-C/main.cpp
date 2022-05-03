361337fdd8b28b53ed37530141c94b730692ac83
Priyansh Saini <103327623+Priyansh4615@users.noreply.github.com>
2022-05-03 18:24:40 +0000

    typecasting , data types

--- .ccls-cache/@home@runner@DSA-C/main.cpp
+++ .ccls-cache/@home@runner@DSA-C/main.cpp
@@ -2,5 +2,52 @@
 using namespace std;
 
 int main() {
+/*
+  
   cout << "Namaste duniya :)"<<endl;
+
+  int a=123;
+  cout << a << endl;
+
+  char b = 'v';
+  cout << b << endl;
+
+  bool bl = true;
+  cout << bl << endl;
+
+  float f = 1.2;
+  cout << f << endl;
+
+  double d = 1.23;
+  cout << d << endl;
+
+  int size1 = sizeof(a);
+  int size2 = sizeof(b);
+  int size3 = sizeof(bl);
+  int size4 = sizeof(f);
+  int size5 = sizeof(d);
+  
+  cout << "Size of a is :" << size1 << endl;
+  cout << "Size of b is :" << size2 << endl;
+  cout << "Size of bl is :" << size3 << endl;
+  cout << "Size of f is :" << size4 << endl;
+  cout << "Size of d is :" << size5 << endl;
+
+
+
+  int x ='x';
+  cout << x << endl;  // 120 (Typecasting)
+
+  char ch = 98;
+  cout << ch << endl; //b (Typecasting)
+
+  char ch1  = 123456;
+  cout << ch1 << endl; //warning (implicit conversion)
+  */
+
+  unsigned int z = -112;
+  unsigned int y = 112;
+  cout << z << endl;
+  cout << y << endl;
+  
 }
